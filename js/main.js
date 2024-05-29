e1a37dee3d513aee439e010cc0aff02de405ee23
MohamedSalman2371999 <95053768+MohamedSalman2371999@users.noreply.github.com>
2024-05-30 01:21:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
 var quotesConainers = [
   {
-    quote: "Beware of what you become in pursuit of what you want.",
+    quote: `"Beware of what you become in pursuit of what you want."`,
     author: "Jim Rohn",
   },
   {
-    quote:
-      "It's not what happens to you, but how you react to it that matters.",
+    quote: `"It's not what happens to you, but how you react to it that matters."`,
     author: "Epictetus",
   },
-  { quote: "The best revenge is massive success.", author: "Frank Sinatra" },
+  { quote: `"The best revenge is massive success."`, author: "Frank Sinatra" },
   {
-    quote: "You miss 100% of the shots you don't take.",
+    quote: `"You miss 100% of the shots you don't take."`,
     author: "Wayne Gretzy",
   },
   {
-    quote:
-      "Resentment is like drinking poison and waiting for your enemies to die.",
+    quote: `"Resentment is like drinking poison and waiting for your enemies to die."`,
     author: "Nelson Mandela",
   },
   {
-    quote: "Do not take life too seriously. You will not get out alive.",
+    quote: `"Do not take life too seriously. You will not get out alive."`,
     author: "Elbert Hubbard",
   },
 ];
-
+var quoteInput = document.getElementById("quoteName");
 function newQuote() {
   var cartona = "";
   var rang = Math.random() * quotesConainers.length;
   var randamNum = Math.floor(rang);
-  cartona = `    
+  var x = quoteInput.innerText;
+  if (x.length != quotesConainers[randamNum].quote.length) {
+    cartona = `    
     <div class="img-container my-4 animate__animated animate__flipInX">
     <img src="images/testimonial-2.jpg" alt="">
     </div>    
-    <h2 class="text-center animate__animated animate__fadeInLeft">"${quotesConainers[randamNum].quote}"</h2>
+    <h2 id="quoteName" class="text-center animate__animated animate__fadeInLeft">"${quotesConainers[randamNum].quote}"</h2>
     <h3 class="text-center pb-4 animate__animated animate__fadeInLeft" >--${quotesConainers[randamNum].author}</h3>
     `;
-  document.getElementById("quotes").innerHTML = cartona;
+
+  } else {
+    console.log("the same len");
+  }
+  document.getElementById("quotes").innerHTML=cartona
 }
